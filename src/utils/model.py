bd78cd27f74c16df056d077b65a9122980fb8b0b
Pourush Kumar Gupta <pkg.21p10161@mtech.nitdgp.ac.in>
2023-11-04 10:44:36 +0530

    update model.py file

---
+++ src/utils/model.py
@@ -0,0 +1,38 @@
+import tensorflow as tf 
+import time 
+import os 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+
+
+def create_model(LOSS_FUNCTION,OPTIMIZER,METRICS,NUM_CLASS):
+    layers=[
+        tf.keras.layers.Flatten(input_shape=[28,28],name="input_layer"),
+        tf.keras.layers.Dense(300,activation="relu",name="first_hidden_layer"),
+        tf.keras.layers.Dense(100,activation="relu",name="second_hidden_layer"),
+        tf.keras.layers.Dense(NUM_CLASS,activation="softmax",name="output_layer")
+    ]
+
+    model_clf= tf.keras.models.Sequential(layers)
+
+    model_clf.summary()
+
+    model_clf.compile(loss=LOSS_FUNCTION,
+                      optimizer=OPTIMIZER,
+                      metrics=METRICS)
+    
+    return model_clf
+
+def create_plots(history):
+    pd.DataFrame(history.history).plot(figsize=(10,7))
+    plt.show()
+
+def get_unique_file_name(filename):
+    unique_file_name = time.strftime(f"%Y%m%d-%H%M%S{filename}")
+    return unique_file_name
+
+def save_model(model,model_name,model_dir):
+    unique_file_name=get_unique_file_name(model_name)
+    path_to_model = os.path.join(model_dir,unique_file_name)
+
+    model.save(path_to_model)
